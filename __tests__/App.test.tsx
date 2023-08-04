7f0a6f190a54582186da7e23efdc95f35abd8167
Davidson Marra <davidsonmarra@gmail.com>
2023-08-04 12:20:20 -0300

    refactor: removing not used mocks

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,14 +1,7 @@
 import App from '../App';
-import { productDetailsApi } from '../__mocks__';
 import { matchSnapshotWithProviders, act } from './test-utils';
 
 jest.mock('../.storybook');
-
-const fetch: any = async () => ({
-  json: async () => productDetailsApi[0]
-});
-
-global.fetch = fetch;
 
 describe('App', () => {
   it('should match a snapshot', async () => {
